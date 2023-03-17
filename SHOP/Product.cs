c78b15d3d3426041367686c861afbffff304b29c
f3xz1 <Gagn_ob80@itstep.edu.az>
2023-03-17 17:27:41 +0400

    Product_List.xaml added

--- SHOP/Product.cs
+++ SHOP/Product.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 
@@ -27,21 +30,18 @@ namespace SHOP
         public BitmapImage pic_bitmap { get; set; }
 
         public string category { get; set; }
-        public Product(string name, double price, double quality, string description)
+        public Product(string name, double price, double quality, string description,string category)
         {
             this.name = name;
             this.price = price;
             this.quality = quality;
             this.description = description;
+            this.category = category;
         }
-        public Product()
-        {
+        //public Product()
+        //{
 
-        }
-        static Product()
-        {
-
-        }
+        //}
         public bool create_product()
         {
             try
@@ -59,10 +59,46 @@ namespace SHOP
             }
             return true;
         }
-        public static void Upload_products()
-        {
 
-        }
+        //private async Task LoadPictureAsync(byte[] pic)
+        //{
+        //    try
+        //    {
+        //        using (var ms = new MemoryStream(pic))
+        //        {
+        //            this.pic_bitmap.BeginInit();
+        //            this.pic_bitmap.StreamSource = ms;
+        //            this.pic_bitmap.EndInit();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        //private byte[] CreateCopy()
+        //{
+        //    Image image = Image.FromFile(FileName);
+        //    int maxWidth = 300,maxHeight = 300;
+        //    double ratioX = (double)maxWidth / image.Width;
+        //    double ratioY = (double)maxHeight / image.Height;
+        //    double ratio = Math.Min(ratioX, ratioY);
+        //    int newWidth = (int)(image.Width * ratio);
+        //    int newHeight = (int)(image.Height * ratio);
+        //    Image myImage = new Bitmap(newWidth, newHeight);
+        //    Graphics graphics = Graphics.FromImage(myImage);
+        //    graphics.DrawImage(image, newWidth, newHeight);
+        //    MemoryStream memorystream = new MemoryStream();
+        //    myImage.Save(memorystream, ImageFormat.Jpeg);
+        //    memorystream.Flush(); 
+        //    BinaryReader binaryReader = new BinaryReader(memorystream);
+        //    byte[] bytes = binaryReader.ReadBytes((int)memorystream.Length);
+        //    return bytes;
+        //}
+
+
+
     }
 }
 //monitor - lock
